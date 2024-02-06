6d25d720d80b09a180843e63c8dfb2e6439272b4
9112291655 <pratikvavhal@MacBook.local>
2024-02-06 13:29:18 +0530

    first commit

---
+++ src/main/java/com/app/agrologs/AgroLogsApplication.java
@@ -0,0 +1,13 @@
+package com.app.agrologs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroLogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroLogsApplication.class, args);
+	}
+
+}
